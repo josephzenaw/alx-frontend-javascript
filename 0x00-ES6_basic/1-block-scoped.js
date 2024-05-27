5a6d3aef53501622355114b09ff8a09b121befa2
josephzenaw <yosephzenaw58@gmail.com>
2024-05-27 18:04:14 +0000

    File Update: 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    var innerTask = true;
-    var innerTask2 = false;
-    task = innerTask;
-    task2 = innerTask2;
-  }
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
